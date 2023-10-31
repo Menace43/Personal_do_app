1bf15cc2222f3421379db65ecabd1e3c18761392
Menace43 <joel_babatunde@yahoo.com>
2023-10-31 18:16:36 -0500

    Second commit

---
+++ Task_frame.py
@@ -0,0 +1,14 @@
+#Author: Adeyemo Joel
+#Date :  10/31/2023 
+#File: Task_frame.py
+#Description: 
+
+
+
+# Dependencies
+
+
+class Task_frame():
+    
+    def __init__(self):
+        pass
--- test/manual_integration_test/test_calendar.py
+++ test/manual_integration_test/test_calendar.py
@@ -1,6 +1,7 @@
 
 
 import re
+from icecream import ic
 
 class Classic_Tests:
     
@@ -13,7 +14,21 @@ class Classic_Tests:
         time_pattern=r"^(\d{1,2}(:\d{2})?)(\s?)([apAp][mM])$"
         print(input_time)
         return bool(re.match(time_pattern,str(input_time)))
+    
+    def clicked_on(self):
+        myvals=Send_value()
+        res=myvals.send_value("present")
+        return res
+
+class Send_value():
+    def send_value(self,new_res):
+        res={
+            "Value1":"1",
+            "new_value":f'{new_res}'
+        }
+        return res
 
 if __name__ =="__main__":
     Tests=Classic_Tests()
-    print(Tests.validate_time_format("12:30 AM"))
+    #print(Tests.validate_time_format("12:00 AM"))
+    ic(Tests.clicked_on())
